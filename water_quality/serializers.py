9a33a15a1865d621fd7d72a0887b8ac992b99388
MayankNCodes <132740070+MayankNCodes@users.noreply.github.com>
2025-09-07 19:06:15 +0530

    Update serializers.py

--- water_quality/serializers.py
+++ water_quality/serializers.py
@@ -2,10 +2,18 @@ from rest_framework import serializers
 from .models import WaterQualitySample
 
 class WaterQualitySampleSerializer(serializers.ModelSerializer):
+    pollution_status = serializers.SerializerMethodField()
+    
     class Meta:
         model = WaterQualitySample
         fields = '__all__'
-        read_only_fields = ('hmpi', 'hpi', 'hei', 'hci', 'cd', 'pi', 'pli', 'created_at', 'updated_at')
+        read_only_fields = (
+            'hmpi', 'hpi', 'hei', 'hci', 'cd', 'pi', 'pli', 
+            'created_at', 'updated_at', 'pollution_status'
+        )
+    
+    def get_pollution_status(self, obj):
+        return obj.get_pollution_status()
     
     def create(self, validated_data):
         sample = super().create(validated_data)
@@ -18,9 +26,16 @@ class WaterQualitySampleSerializer(serializers.ModelSerializer):
         return sample
 
 class WaterQualityReportSerializer(serializers.ModelSerializer):
+    pollution_status = serializers.SerializerMethodField()
+    
     class Meta:
         model = WaterQualitySample
         fields = [
             'sample_id', 'sampling_date', 'latitude', 'longitude', 'well_depth',
-            'hmpi', 'hpi', 'hei', 'hci', 'cd', 'pi', 'pli'
+            'lead', 'cadmium', 'chromium', 'arsenic', 'mercury', 'nickel', 
+            'copper', 'zinc', 'iron', 'manganese', 'cobalt',
+            'hmpi', 'hpi', 'hei', 'hci', 'cd', 'pi', 'pli', 'pollution_status'
         ]
+    
+    def get_pollution_status(self, obj):
+        return obj.get_pollution_status()
